d5d81a87ca7ad7add14dd60e8017fe924d7f45b5
AndyOK <andyortegakou@yahoo.com.mx>
2025-07-15 23:26:16 -0600

    feat: error handling

--- program/Driver.py
+++ program/Driver.py
@@ -8,7 +8,12 @@ def main(argv):
     lexer = MiniLangLexer(input_stream)
     stream = CommonTokenStream(lexer)
     parser = MiniLangParser(stream)
-    tree = parser.prog()  # We are using 'prog' since this is the starting rule based on our MiniLang grammar, yay!
+
+    try:
+        tree = parser.prog()  # We are using 'prog' since this is the starting rule based on our MiniLang grammar, yay!
+        print("Parsing successful (no syntax errors).")
+    except Exception as e:
+        print("Parsing failed with error:", e)
 
 if __name__ == '__main__':
     main(sys.argv)
